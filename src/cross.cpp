a2ef5457e8399787744a124b2ddcc90a2cf7f0cc
Augustin DESVALLEES <adesvall@e2r10p3.clusters.42paris.fr>
2023-03-03 11:58:41 +0100

    lwfmw

--- src/cross.cpp
+++ src/cross.cpp
@@ -18,6 +18,15 @@ vector<string> astar_for_cross(Cube& cube)
         string str = current.cube.to_string_forcross();
         if (visited.find(str) != visited.end())
             continue;
+        if (current.cube.is_cross())
+        {
+            cout << "Found cross!" << endl;
+            for (string move : current.moves)
+                Cube::res_moves += move + " ";
+            // Cube::res_moves += "\n";
+            cube = current.cube;
+            return current.moves;
+        }
         visited.insert(str);
         for (char face : Cube::face_names)
         {
@@ -27,15 +36,6 @@ vector<string> astar_for_cross(Cube& cube)
                 next.rotate(face, i);
                 vector<string> moves = current.moves;
                 moves.push_back(string(1, face+'A'-'a') + (i==2?"'":i==2?"2":""));
-                if (next.is_cross())
-                {
-                    cout << "Found cross!" << endl;
-                    for (string move : moves)
-                        Cube::res_moves += move + " ";
-                    // Cube::res_moves += "\n";
-                    cube = next;
-                    return moves;
-                }
                 queue.push(SearchCube(next, moves));
             }
         }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 
 #include "../Cubos.hpp"
 
-void test_res()
+void test_res(Cube rk)
 {
-    Cube rk;
-    string res = formula_cleaner(Cube::res_moves);
+    string res = Cube::res_moves;
+    // string res = formula_cleaner(Cube::res_moves);
     cout << res << endl;
     rk.apply_moves(res);
     print_ascii_rubik(rk);
@@ -23,18 +23,25 @@ int main()
     // cout << "PLL algos checked." << endl;
     // exit(0);
 
-
     for (int i = 0; i < 1; ++i)
     {
         rk.shuffle();
+        Cube cpy = rk;
+        print_ascii_rubik(rk);
+
         astar_for_cross(rk);
-        test_res();
+        test_res(cpy);
+
+        exit(1);
+        cpy = rk;
         rk.solve_2FL_v2();
-        test_res();
+        test_res(cpy);
+        cpy = rk;
         rk.solve_OLL();
-        test_res();
+        test_res(cpy);
+        cpy = rk;
         rk.solve_PLL();
-        test_res();
+        test_res(cpy);
     }
 
 }
